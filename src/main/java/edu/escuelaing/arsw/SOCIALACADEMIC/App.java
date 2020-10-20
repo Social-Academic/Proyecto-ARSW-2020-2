6bd20e63775b6e75afee1e51c6fdd63d8454e39a
Jose Luis Gomez Camacho <josemania10@gmail.com>
2020-10-19 23:29:15 -0500

    Actualizacion del procfile y pom

--- src/main/java/edu/escuelaing/arsw/SOCIALACADEMIC/App.java
+++ src/main/java/edu/escuelaing/arsw/SOCIALACADEMIC/App.java
@@ -1,6 +1,5 @@
 package edu.escuelaing.arsw.SOCIALACADEMIC;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
